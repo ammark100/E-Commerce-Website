bcf9d1338cacda39fa428e3dcf656b8cc7940785
Ammar Khan <ammark100@gmail.com>
2017-07-24 01:12:16 +0500

    Adding a validation of quantity

--- 11March2017Web/Controllers/ProductController.cs
+++ 11March2017Web/Controllers/ProductController.cs
@@ -36,31 +36,47 @@ namespace _11March2017Web.Controllers
 
             Cart temp_cart = new Cart();
 
-            if (ModelState.IsValid)
+            if (prod.productQuantity > product.productQuantity)
             {
-                temp_cart.productQty = prod.productQuantity == null ? 1 : prod.productQuantity;
-            }
-            
-            temp_cart.productID = product.productID;
-            temp_cart.productName = product.productName;
-            temp_cart.productUnitPrice = product.productPrice;
-            temp_cart.productQty = prod.productQuantity == null ? 1 : prod.productQuantity;
-            temp_cart.productTotalPrice = temp_cart.productUnitPrice * (float)temp_cart.productQty;
-            temp_cart.vendorID = 3;
+                ModelState.AddModelError("productQuantity", "Sorry, We do not have your required quantity");
+                List<tblImage> img = db.tblImages.Where(prodID => prodID.prod_ID == product.productID).ToList();
+                product.tblImages = img;
 
-            if (product.productThumbnailPath == null)
-            {
-                temp_cart.productThumbnailPath = "~/Content/img/noImage.jpg";
+
+                ViewBag.productCategory = new SelectList(db.tblCategories, "categoryID", "categoryName", product.productCategory);
+                return View(product);
             }
+
+            /*  if (ModelState.IsValid)
+              {
+                  temp_cart.productQty = prod.productQuantity == null ? 1 : prod.productQuantity;
+              }*/
             else
             {
-                temp_cart.productThumbnailPath = product.productThumbnailPath;
+
+
+
+                temp_cart.productID = product.productID;
+                temp_cart.productName = product.productName;
+                temp_cart.productUnitPrice = product.productPrice;
+                temp_cart.productQty = prod.productQuantity == null ? 1 : prod.productQuantity;
+                temp_cart.productTotalPrice = temp_cart.productUnitPrice * (float)temp_cart.productQty;
+                temp_cart.vendorID = 3;
+
+                if (product.productThumbnailPath == null)
+                {
+                    temp_cart.productThumbnailPath = "~/Content/img/noImage.jpg";
+                }
+                else
+                {
+                    temp_cart.productThumbnailPath = product.productThumbnailPath;
+                }
+
+                cart_list.Add(temp_cart);
+                Session["my_cart"] = cart_list;
+
+                return RedirectToAction("Shopping_cart", "Order");
             }
-
-            cart_list.Add(temp_cart);
-            Session["my_cart"] = cart_list;
-
-            return RedirectToAction("Shopping_cart", "Order");
         }
     }
 }
